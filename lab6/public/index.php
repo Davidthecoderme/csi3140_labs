8a804ce4ed8229b92d6f3de6a54c220b55cf85ec
D-codeTillDie <125109506+Davidthecoderme@users.noreply.github.com>
2024-06-26 21:05:15 -0400

    update for YatzyEngine

--- lab6/public/index.php
+++ lab6/public/index.php
@@ -17,3 +17,12 @@ for ($i = 1; $i <= 3; $i++) {
     $game->rollDice();
     echo "ROLL {$i}: " . implode(", ", $game->getDiceValues()) . "<br>";
 }
+
+
+echo "<h2>Yatzy Engine Scores</h2>";
+echo "Score for ones: " . YatzyEngine::calculateScore($game, 'ones') . "<br>";
+echo "Score for twos: " . YatzyEngine::calculateScore($game, 'twos') . "<br>";
+echo "Score for threes: " . YatzyEngine::calculateScore($game, 'threes') . "<br>";
+echo "Score for fours: " . YatzyEngine::calculateScore($game, 'fours') . "<br>";
+echo "Score for fives: " . YatzyEngine::calculateScore($game, 'fives') . "<br>";
+echo "Score for sixes: " . YatzyEngine::calculateScore($game, 'sixes') . "<br>";
